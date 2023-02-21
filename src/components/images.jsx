94950ad326388262e6e07411beaac6002f5dbdfb
D4NTE-DB <dantebabilonia98@gmail.com>
2023-02-21 12:22:11 -0500

    first

---
+++ src/components/images.jsx
@@ -0,0 +1,24 @@
+const IMG = [
+    {
+        id: 1,
+        image1: 'images/AMPLIACION SPA UÑAS/1.png',
+        image2: 'images/AMPLIACION SPA UÑAS/2.png',
+        image3: 'images/AMPLIACION SPA UÑAS/3.png'
+    },
+    {
+        id: 2,
+        image1: 'images/AMPLIACION SPA UÑAS/1.png',
+        image2: 'images/AMPLIACION SPA UÑAS/2.png',
+        image3: 'images/AMPLIACION SPA UÑAS/3.png'
+    },
+    {
+        id: 3,
+        image1: 'images/AMPLIACION SPA UÑAS/1.png',
+        image2: 'images/AMPLIACION SPA UÑAS/2.png',
+        image3: 'images/AMPLIACION SPA UÑAS/3.png'
+    },
+
+
+]
+
+export default IMG;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,176 +3,75 @@ import { Card, Carousel, ListGroup } from 'react-bootstrap';
 import first1 from '../../public/images/AMPLIACION SPA UÑAS/1.png'
 import first2 from '../../public/images/AMPLIACION SPA UÑAS/1.png'
 import first3 from '../../public/images/AMPLIACION SPA UÑAS/1.png'
+import IMG from '../components/images';
+
+
 
 const Products = () => {
+
+    console.log(IMG.SPA)
+
     return (
         <div className='products'>
-        <Card style={{ width: '20rem' }}>
-            
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first1}
-                        alt="first1 slide"
-                    />
-                    <Carousel.Caption>
-                        {/* <h3>first1 slide label</h3> */}
-                        {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first2}
-                        alt="Second slide"
-                    />
+            {IMG.map((data) => (
+                <Card style={{ width: '20rem' }} key={data.id}>
 
-                    <Carousel.Caption>
-                        {/* <h3>Second slide label</h3> */}
-                        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first3}
-                        alt="Third slide"
-                    />
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={data.image1}
+                                alt="first1 slide"
+                            />
+                            <Carousel.Caption>
+                                {/* <h3>first1 slide label</h3> */}
+                                {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={data.image2}
+                                alt="Second slide"
+                            />
 
-                    <Carousel.Caption>
-                        {/* <h3>Third slide label</h3> */}
-                        {/* <p> */}
-                        {/* Praesent commodo cursus magna, vel scelerisque nisl consectetur. */}
-                        {/* </p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+                            <Carousel.Caption>
+                                {/* <h3>Second slide label</h3> */}
+                                {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={data.image3}
+                                alt="Third slide"
+                            />
 
-            <Card.Body>
-                <Card.Title>Diseño 1</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item>Materiales</ListGroup.Item>
-                <ListGroup.Item>Acabados</ListGroup.Item>
-                {/* <ListGroup.Item>Vestibulum at eros</ListGroup.Item> */}
-            </ListGroup>
+                            <Carousel.Caption>
+                                {/* <h3>Third slide label</h3> */}
+                                {/* <p> */}
+                                {/* Praesent commodo cursus magna, vel scelerisque nisl consectetur. */}
+                                {/* </p> */}
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
 
-        </Card>
-        <Card style={{ width: '20rem' }}>
-        <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first1}
-                        alt="first1 slide"
-                    />
-                    <Carousel.Caption>
-                        {/* <h3>first1 slide label</h3> */}
-                        {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first2}
-                        alt="Second slide"
-                    />
+                    <Card.Body>
+                        <Card.Title>Diseño 1</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroup.Item>Materiales</ListGroup.Item>
+                        <ListGroup.Item>Acabados</ListGroup.Item>
+                        {/* <ListGroup.Item>Vestibulum at eros</ListGroup.Item> */}
+                    </ListGroup>
 
-                    <Carousel.Caption>
-                        {/* <h3>Second slide label</h3> */}
-                        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first3}
-                        alt="Third slide"
-                    />
+                </Card>
+            ))}
 
-                    <Carousel.Caption>
-                        {/* <h3>Third slide label</h3> */}
-                        {/* <p> */}
-                        {/* Praesent commodo cursus magna, vel scelerisque nisl consectetur. */}
-                        {/* </p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-
-            <Card.Body>
-                <Card.Title>Diseño 1</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item>Materiales</ListGroup.Item>
-                <ListGroup.Item>Acabados</ListGroup.Item>
-                {/* <ListGroup.Item>Vestibulum at eros</ListGroup.Item> */}
-            </ListGroup>
-        </Card>
-        <Card style={{ width: '20rem' }}>
-
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first1}
-                        alt="first1 slide"
-                    />
-                    <Carousel.Caption>
-                        {/* <h3>first1 slide label</h3> */}
-                        {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first2}
-                        alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                        {/* <h3>Second slide label</h3> */}
-                        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={first3}
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        {/* <h3>Third slide label</h3> */}
-                        {/* <p> */}
-                        {/* Praesent commodo cursus magna, vel scelerisque nisl consectetur. */}
-                        {/* </p> */}
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-
-            <Card.Body>
-                <Card.Title>Diseño 1</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item>Materiales</ListGroup.Item>
-                <ListGroup.Item>Acabados</ListGroup.Item>
-                {/* <ListGroup.Item>Vestibulum at eros</ListGroup.Item> */}
-            </ListGroup>
-        
-        </Card>
         </div>
     );
 };
